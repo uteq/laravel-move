62e9c9f01d08328dfc61bf3ccd3beabf34ecf41d
Nathan Jansen <info@nathanjansen.nl>
2020-10-28 15:10:20 +0100

    Changed run tests os and dependency version so it will pass

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Uteq\Move\Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class UserFactory extends Factory
+{
+    protected $model = User::class;
+
+    public function definition()
+    {
+        return [
+
+        ];
+    }
+}
