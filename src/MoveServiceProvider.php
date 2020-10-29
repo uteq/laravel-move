f906fe6277b663bf76415ae30cee080dd81f5457
Nathan Jansen <info@nathanjansen.nl>
2020-10-29 11:43:02 +0100

    Disabled table move migration because it is not used for now

--- src/MoveServiceProvider.php
+++ src/MoveServiceProvider.php
@@ -153,12 +153,12 @@ class MoveServiceProvider extends ServiceProvider
             __DIR__ . '/../resources/views' => base_path('resources/views/vendor/move'),
         ], 'views');
 
-        $migrationFileName = 'create_laravel_move_table.php';
-        if (! $this->migrationFileExists($migrationFileName)) {
-            $this->publishes([
-                __DIR__ . "/../database/migrations/{$migrationFileName}.stub" => database_path('migrations/' . date('Y_m_d_His', time()) . '_' . $migrationFileName),
-            ], 'migrations');
-        }
+//        $migrationFileName = 'create_move_table.php';
+//        if (! $this->migrationFileExists($migrationFileName)) {
+//            $this->publishes([
+//                __DIR__ . "/../database/migrations/{$migrationFileName}.stub" => database_path('migrations/' . date('Y_m_d_His', time()) . '_' . $migrationFileName),
+//            ], 'migrations');
+//        }
     }
 
     public static function migrationFileExists(string $migrationFileName): bool
