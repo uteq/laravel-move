ea0fd87a081d575244a933a43d5fd02cf9429005
stef1904berg <55401301+stef1904berg@users.noreply.github.com>
2021-01-08 15:33:39 +0100

    numberFormat() function added to number field

--- src/Fields/Number.php
+++ src/Fields/Number.php
@@ -6,4 +6,23 @@ namespace Uteq\Move\Fields;
 class Number extends Field
 {
     public string $component = 'number-field';
+
+    public function numberFormat(int $decimals = 0, string $decimalSeparator = '.', string $thousandSeparator = ','): self
+    {
+        $this->displayCallback = function ($value, $resource, $attribute) use ($decimals, $decimalSeparator, $thousandSeparator) {
+            return $value
+                ? number_format((float)$value, $decimals, $decimalSeparator, $thousandSeparator)
+                : null;
+        };
+
+        $this->beforeStore(function ($value, $field, $model, $data) use ($decimals) {
+            $value = str_replace('.', '', $value);
+            $value = str_replace(',', '.', $value);
+            return $value
+                ? number_format((float)$value, $decimals, '.', '')
+                : null;
+        });
+
+        return $this;
+    }
 }
