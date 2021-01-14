6359cae0cf6761b826cf9e076ac173433166d3a0
Nathan Jansen <info@nathanjansen.nl>
2021-01-14 10:01:24 +0100

    Fix for input allowing wire:model

--- resources/views/components/field/input.blade.php
+++ resources/views/components/field/input.blade.php
@@ -1,9 +1,20 @@
-@props(['disabled' => false, 'model'])
+@props(['disabled' => false, 'model' => null, 'updating' => null, 'debounce' => '500ms'])
+
+@php
+    $modelId = $model ?: $attributes->wire('model')->value();
+@endphp
 
 <input {{ $disabled ? 'disabled' : '' }}
-       id="{{ $model }}"
-       wire:model.defer="{{ $model }}"
-       autocomplete="{{ $model }}"
+    id="{{ $modelId }}"
+
+    @if ($attributes->wire('model')->value())
+        {{ $attributes->wire('model') }}
+    @else
+        wire:model="{{ $model }}"
+    @endif
+
+    autocomplete="{{ $modelId }}"
+
     {!! $attributes->merge([
         'type' => 'text',
         'class' => 'flex-1 form-input block w-full min-w-0 rounded-none rounded-md transition duration-150 ease-in-out sm:text-sm sm:leading-5 border-gray-300 rounded'
