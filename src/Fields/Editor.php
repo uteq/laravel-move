643c9973677e4effe4eac04d1412cbb72d1e91b7
Nathan Jansen <info@nathanjansen.nl>
2021-02-04 11:17:18 +0100

    Fix for editor utf8 encoding

--- src/Fields/Editor.php
+++ src/Fields/Editor.php
@@ -2,6 +2,8 @@
 
 namespace Uteq\Move\Fields;
 
+use Uteq\Move\Support\Encoding;
+
 class Editor extends Textarea
 {
     public string $component = 'editor-field';
@@ -15,6 +17,15 @@ class Editor extends Textarea
     {
         if (mb_detect_encoding(mb_convert_encoding($string, 'Windows-1251', 'UTF-8'), 'Windows-1251,UTF-8', true) === 'UTF-8') {
             return utf8_decode($string);
+        }
+
+        try {
+            $string = Encoding::fixUTF8($string, Encoding::ICONV_TRANSLIT);
+        } catch (\ErrorException $e) {
+            // Assuming TRANSLIT threw an error because of iconv
+            //  https://stackoverflow.com/questions/8727735/iconv-detected-an-illegal-character-in-input-string
+            //  we now return to simple decode and using iconv ignore.
+            $string = Encoding::fixUTF8(utf8_decode($string), Encoding::ICONV_IGNORE);
         }
 
         return $string;
