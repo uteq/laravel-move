0cb6a5ca5f5ff6e0d49b6b276f31688e0a887b0e
Nathan Jansen <info@nathanjansen.nl>
2021-07-28 11:11:42 +0200

    wip

--- src/Fields/HasOne.php
+++ src/Fields/HasOne.php
@@ -8,9 +8,17 @@ class HasOne extends Select
 {
     use HasResource;
 
-    public function __construct(string $name, string $attribute = null, string $resource = null, \Closure $valueCallback)
+    public function __construct(string $name, string $attribute, string $resource, \Closure $valueCallback = null)
     {
         $this->resourceName = $this->findResourceName($name, $resource);
+
+        $valueCallback ??= function($value, $model, $field) use ($resource) {
+            if (\request()->input('parent_model') !== $resource::$model) {
+                return $value;
+            }
+
+            return \request()->input('parent_id', $value);
+        };
 
         parent::__construct($name, $attribute, $valueCallback);
     }
