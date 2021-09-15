6142103096e191887b697454625344f943ea82dd
Nathan Jansen <info@nathanjansen.nl>
2021-09-15 11:37:49 +0200

    Fixes for images

--- src/File/ResourceFile.php
+++ src/File/ResourceFile.php
@@ -22,25 +22,33 @@ class ResourceFile extends File implements ResourceFileContract
 
     public function getPath()
     {
-        if (! in_array($this->media->disk, ['local', 'public'])) {
-            $path = str_replace(
-                DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR,
-                DIRECTORY_SEPARATOR,
-                sys_get_temp_dir() . DIRECTORY_SEPARATOR . $this->media->uuid
-            );
+        $path = $this->media->getPath();
 
-            if (! file_exists($path)) {
-                $dir = dirname($path);
-                if (! file_exists($dir)) {
-                    mkdir($dir, null, true);
-                }
-                file_put_contents($path, file_get_contents($this->media->getFullUrl()));
-            }
-
+        if (file_exists($path)) {
             return $path;
-        } else {
-            return $this->media->getPath();
         }
+
+        $path = str_replace(
+            DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR,
+            DIRECTORY_SEPARATOR,
+            sys_get_temp_dir() . DIRECTORY_SEPARATOR . $this->media->uuid
+        );
+
+        if (file_exists($path)) {
+            return $path;
+        }
+
+        $dir = dirname($path);
+
+        if (! file_exists($dir)) {
+            if (!mkdir($dir, null, true) && !is_dir($dir)) {
+                throw new \RuntimeException(sprintf('Directory "%s" was not created', $dir));
+            }
+        }
+
+        file_put_contents($path, file_get_contents($this->media->getFullUrl()));
+
+        return $path;
     }
 
     public function exists()
