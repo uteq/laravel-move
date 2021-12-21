2169ab7fd56a74a23c043729917795dd6e8c17eb
Nathan Jansen <info@nathanjansen.nl>
2021-12-21 17:38:25 +0100

    Updated to php cs fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -20,7 +20,7 @@ return (new PhpCsFixer\Config)
         'ordered_imports' => ['sortAlgorithm' => 'alpha'],
         'no_unused_imports' => true,
         'not_operator_with_successor_space' => true,
-        'trailing_comma_in_multiline_array' => true,
+        'trailing_comma_in_multiline' => true,
         'phpdoc_scalar' => true,
         'unary_operator_spaces' => true,
         'binary_operator_spaces' => true,
@@ -29,11 +29,6 @@ return (new PhpCsFixer\Config)
         ],
         'phpdoc_single_line_var_spacing' => true,
         'phpdoc_var_without_name' => true,
-        'class_attributes_separation' => [
-            'elements' => [
-                'method',
-            ],
-        ],
         'method_argument_space' => [
             'on_multiline' => 'ensure_fully_multiline',
             'keep_multiple_spaces_after_comma' => true,
