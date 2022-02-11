cf07afb2f1c53fedeee261e40aa0078ed3fd9bf9
Nathan Jansen <info@nathanjansen.nl>
2022-02-11 11:21:49 +0100

    Changed radio group coloring to be beter readable

--- resources/views/form/radio-group.blade.php
+++ resources/views/form/radio-group.blade.php
@@ -12,10 +12,10 @@
         @foreach ($field->getOptions() as $value => $description)
             <label
                 class="
-                    {{ $loop->first ? 'rounded-l-lg' : 'border-l-0 -ml-px' }}
-                    {{ $loop->last ? 'rounded-r-lg' : 'border-r-0' }}
-                    {{ $field->value === $value ? 'bg-primary-100 text-primary-900' : 'bg-white hover:bg-gray-100' }}
-                    relative inline-flex items-center block border border-gray-300 shadow-sm px-4 py-1.5 cursor-pointer hover:border-gray-400 sm:flex sm:justify-between focus:z-10 focus:outline-none focus:ring-1 focus-within:ring-primary-900 focus:border-primary-900
+                    {{ $loop->first ? 'rounded-l-lg' : ' -ml-px' }}
+                    {{ $loop->last ? 'rounded-r-lg' : '' }}
+                    {{ $field->value === $value ? 'bg-white hover:bg-gray-100 border-primary-500' : 'bg-white text-primary-900 opacity-75 hover:border-primary-400 hover:opacity-100' }}
+                    transition relative inline-flex items-center block border border-gray-300 shadow-sm px-4 py-1.5 cursor-pointer sm:flex sm:justify-between focus:z-10 focus:outline-none focus:ring-1 focus-within:ring-primary-900 focus:border-primary-900
                 "
                 <?php if ($field->value !== $value): ?> :class="{ 'bg-gray-100': active }" <?php endif; ?>
                 x-data="{ active : {{ $field->value !== $value ? 'false' : 'true' }} }"
