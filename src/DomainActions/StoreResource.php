8bb842cf8027dd747085280d53fc75b0e551c751
Marnick <contact@marnick.dev>
2022-02-17 14:43:54 +0100

    Migration en nog wat dingen

--- src/DomainActions/StoreResource.php
+++ src/DomainActions/StoreResource.php
@@ -15,6 +15,7 @@ class StoreResource
 
     public function __invoke(Model $model, array $data, Resource $resource)
     {
+        dd('sfd');
         $model = $this->fill($model, $data, $resource, $this->withoutMedia($model, $data));
         $model->save();
 
