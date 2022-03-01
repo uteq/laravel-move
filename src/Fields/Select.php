292c7081472955ab916efaf05706e3a9b9ed0c52
nathanjansen <nathanjansen@users.noreply.github.com>
2022-03-01 14:35:18 +0000

    Fix styling

--- src/Fields/Select.php
+++ src/Fields/Select.php
@@ -388,14 +388,16 @@ class Select extends Field
 
         return $this;
     }
-    
+
     public function optionsBy(string $key, string $value, string $resource = null)
     {
         $resource ??= $this->resource::class;
 
         return $this
             ->shouldMapTags(false)
-            ->index(fn ($field) => implode(', ', $resource::query()
+            ->index(fn ($field) => implode(
+                ', ',
+                $resource::query()
                 ->whereIn($key, $field->value)
                 ->pluck($value)
                 ->toArray()
