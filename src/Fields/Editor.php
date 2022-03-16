d6037537036475bc35e3fec40bdc57b59450afe7
Nathan Jansen <info@nathanjansen.nl>
2022-03-16 15:01:15 +0100

    Removed UTF-8 fix because it created problems

--- src/Fields/Editor.php
+++ src/Fields/Editor.php
@@ -55,21 +55,6 @@ class Editor extends Textarea
 
     public function fixUTF8($string): string
     {
-        ray($string);
-
-        if (mb_detect_encoding(mb_convert_encoding($string, 'Windows-1251', 'UTF-8'), 'Windows-1251,UTF-8', true) === 'UTF-8') {
-            return utf8_decode($string);
-        }
-
-        try {
-            $string = Encoding::fixUTF8($string, Encoding::ICONV_TRANSLIT);
-        } catch (\ErrorException $e) {
-            // Assuming TRANSLIT threw an error because of iconv
-            //  https://stackoverflow.com/questions/8727735/iconv-detected-an-illegal-character-in-input-string
-            //  we now return to simple decode and using iconv ignore.
-            $string = Encoding::fixUTF8(utf8_decode($string), Encoding::ICONV_IGNORE);
-        }
-
         return $string;
     }
 }
