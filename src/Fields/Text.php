984206694f7bfdaa5b0e2f2274c9eed2ee1de72b
Nathan Jansen <info@nathanjansen.nl>
2022-04-14 12:49:02 +0200

    Updated livewire

--- src/Fields/Text.php
+++ src/Fields/Text.php
@@ -2,7 +2,25 @@
 
 namespace Uteq\Move\Fields;
 
+use Closure;
+
 class Text extends Field
 {
     public string $component = 'text-field';
+
+    protected ?Closure $externalLinkCallback = null;
+
+    public function externalLink(Closure $closure): static
+    {
+        $this->externalLinkCallback = $closure;
+
+        return $this;
+    }
+
+    public function getExternalLink(array $args = []): ?string
+    {
+        return $this->externalLinkCallback
+            ? ($this->externalLinkCallback)($this, $args)
+            : null;
+    }
 }
